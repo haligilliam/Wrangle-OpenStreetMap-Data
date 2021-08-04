e343429293024951f5c5e211fe4439d64d994ba9
haligilliam <87688211+haligilliam@users.noreply.github.com>
2021-08-03 19:55:59 -0700

    Update data.py

--- data.py
+++ data.py
@@ -68,7 +68,7 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                 node_tag['type'] = child.attrib['k'].split(':',1)[0]
                 node_tag['key'] = child.attrib['k'].split(':',1)[1]
                 node_tag['id'] = element.attrib['id']
-                node_tag['value'] = child.attrib['v']
+                node_tag['value'] = update_name(child.attrib['v'])
                 tags.append(node_tag)
             elif PROBLEMCHARS.match(child.attrib['k']):
                 continue
@@ -96,7 +96,7 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                     way_tag['type'] = child.attrib['k'].split(':',1)[0]
                     way_tag['key'] = child.attrib['k'].split(':',1)[1]
                     way_tag['id'] = element.attrib['id']
-                    way_tag['value'] = child.attrib['v']
+                    node_tag['value'] = update_name(child.attrib['v'])
                     tags.append(way_tag)
                 elif PROBLEMCHARS.match(child.attrib['k']):
                     continue
